5d21b9ef76b20f08efc5eb6c0fa990557f48b0a9
Farhana Munni <minniekader@gmail.com>
2019-05-19 09:38:44 -0600

    fallingpath

---
+++ fallingPathReadme.md
@@ -0,0 +1,20 @@
+### Recurrence:
+
+```
+minFallPath(row,col){
+	if (row>=N) return 0;
+	res=inf;
+
+	for (j=0;j<N;j++){
+		if (abs(col-j)<2){
+			res=min(res,A[row][col]+minFallPath(row+1,j))
+		}
+	}
+	return res;
+
+}
+```
+
+// pick max for the col in first row minFallPath(0,col), where col=0,N-1
+Subproblem:
+I think I need a grid to store the solution like dp[row,col]. Dp[row][col] stores min value for the path starting at row `row` and column `col`
---
+++ fallingpath.java
@@ -0,0 +1,46 @@
+import java.util.*;
+//import java.math.*;
+
+class FallingPath {
+    public static int A[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    public static int dp[][];
+    static final int inf = 1 << 29;
+
+    public static void main(String[] args) {
+        int N = A.length;
+        dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], inf);
+        }
+        int res = inf;
+        for (int col = 0; col < N; col++)
+            res = Math.min(res, minFallPath(0, col));
+        System.out.println(res);
+    }
+
+    public static int minFallPath(int row, int col) {
+
+        if (row >= A.length)
+            return 0;
+        if (dp[row][col] < inf)
+            return dp[row][col];
+        int res = inf;
+
+        for (int j = 0; j < A.length; j++) {
+            if (Math.abs(col - j) < 2) {
+                res = Math.min(res, A[row][col] + minFallPath(row + 1, j));
+            }
+        }
+        dp[row][col] = res;
+        return res;
+
+    }
+
+    public static int sum(int i, int j) {
+        int s = 0;
+        for (int k = i; k <= j; k++)
+            s += stones[k];
+        return s;
+    }
+}
